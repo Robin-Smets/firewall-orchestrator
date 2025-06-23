64f67c0721122381f2f5f68bb382d0ec171cb96d
Robin-Smets <robinsmets@outlook.de>
2025-06-23 20:34:23 +0200

    issue 3209 - display ordered layers headers

--- roles/lib/files/FWO.Data/Report/TreeNode.cs
+++ roles/lib/files/FWO.Data/Report/TreeNode.cs
@@ -8,12 +8,14 @@ namespace FWO.Data.Report
         public bool IsExpanded { get; set; } = false;
         public List<TreeNode<T>> Children { get; set; } = [];
         public RulebaseType Type { get; set; }
+        public bool IsCollapsible { get; set; } = false;
+        public string Header { get; set; } = "";
 
         public TreeNode()
         {
-            
+
         }
-        
+
         public TreeNode(ITreeItem<T> treeItem)
         {
             Item = treeItem.Data;
